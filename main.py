8fe2d07eb75920965c6d38199f2209706206465a
Benjamin Guffey <mrben.guffey@gmail.com>
2020-04-09 18:14:35 -0500

    Did the birthday attack correctly this time.

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Executes the birthday attack against the BadHash40 hash function')
     parser.add_argument('-lhash', type=int,
-                        help='Length of hash input/output list', default=100000)
+                        help='Length of hash input/output list', default=1000000)
 
     args = parser.parse_args()
 
@@ -38,34 +38,41 @@ if __name__ == "__main__":
         if os.path.exists("hash.data"):
             os.remove("hash.data")
         else : print ("The file doesn't exist")
-        message_file = open("hash.data","w")   
+        message_file = open("hash.data","w")
         runs += 1
-        value_pairs = {}
-        counter = 0
+        message_list=[]
+        badHash_list=[]
         print ("Starting run: "+str(runs))
-        while counter < args.lhash:
-            counter += 1
 
-            #Call Message Generation
+        counter = 0
+        while counter < args.lhash: #Compile list of key and message pairs
+            counter += 1
             message = genMessage()
             message_file.write(str(message))
+            message_list.append(message)
 
-            #Call BadHash40
             badHash = BadHash40(message)
             message_file.write(' | '+str(badHash)+'\n')
+            badHash_list.append(badHash)
 
-            #Check for duplicate
-            collision = collisionCheck(value_pairs,badHash)
-            if collision == True:
-                break
-            else:
-                value_pairs[badHash] = message
         message_file.close()
-        if collision == True:
+
+        seen = set()
+        seen2 = []
+        for badHash in badHash_list:
+            if badHash in seen:
+                seen2.append(badHash)
+            else:
+                seen.add(badHash)
+        if seen2:
             break
 
     #Output 2 original messages and collision on hash
     print ("It took "+str(runs)+" runs of "+str(args.lhash)+" messages to find a collision.")
-    print ("First  message: "+str(value_pairs[badHash]))
-    print ("Second message: "+str(message))
-    print ("Duplicate Hash: 0x"+str(badHash))
+
+    message1_idx = badHash_list.index(seen2[0])
+    message2_idx = badHash_list[(message1_idx+1):].index(seen2[0])+message1_idx+1
+
+    print ("First  message: "+str(message_list[message1_idx]))
+    print ("Second message: "+str(message_list[message2_idx]))
+    print ("Duplicate Hash: 0x"+str(seen2[0]))
