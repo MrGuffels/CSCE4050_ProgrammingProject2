cb31ae16ac92ef29037dedb3fa5b5b3ac548f616
Benjamin Guffey <mrben.guffey@gmail.com>
2020-04-09 19:18:33 -0500

    Added comments

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    #Start while Loop
+    #Start while True Loop
     runs = 0
     while True:
         #File management for storing messages for use while testing. Resets each run.
@@ -39,40 +39,43 @@ if __name__ == "__main__":
             os.remove("hash.data")
         else : print ("The file doesn't exist")
         message_file = open("hash.data","w")
-        runs += 1
-        message_list=[]
+
+        message_list=[] #Create empty lists for storing the message and hashes
         badHash_list=[]
+
+        runs += 1   #run counter for visual clarity
         print ("Starting run: "+str(runs))
 
-        counter = 0
+        counter = 0 #message counter for list size
+
         while counter < args.lhash: #Compile list of key and message pairs
             counter += 1
             message = genMessage()
-            message_file.write(str(message))
-            message_list.append(message)
+            message_file.write(str(message))    #Write message to file
+            message_list.append(message)        #Add message to list
 
             badHash = BadHash40(message)
-            message_file.write(' | '+str(badHash)+'\n')
-            badHash_list.append(badHash)
+            message_file.write(' | '+str(badHash)+'\n') #Write hash to file
+            badHash_list.append(badHash)        #Add hash to list
 
-        message_file.close()
+        message_file.close()        #Close file writing
 
-        seen = set()
+        seen = set()    #Duplicate testing
         seen2 = []
-        for badHash in badHash_list:
-            if badHash in seen:
-                seen2.append(badHash)
-            else:
-                seen.add(badHash)
-        if seen2:
+        for badHash in badHash_list:    #Loop through hashes in list
+            if badHash in seen:         #If hash in set already
+                seen2.append(badHash)   #Add to duplicate set
+            else:  
+                seen.add(badHash)       #Add to unique set
+        if seen2:       #If duplicate set has elements, break
             break
 
     #Output 2 original messages and collision on hash
     print ("It took "+str(runs)+" runs of "+str(args.lhash)+" messages to find a collision.")
 
-    message1_idx = badHash_list.index(seen2[0])
-    message2_idx = badHash_list[(message1_idx+1):].index(seen2[0])+message1_idx+1
+    message1_idx = badHash_list.index(seen2[0])     #Find index to print message that matches badHash
+    message2_idx = badHash_list[(message1_idx+1):].index(seen2[0])+message1_idx+1       #Find second index to print second message that matches badHash
 
-    print ("First  message: "+str(message_list[message1_idx]))
+    print ("First  message: "+str(message_list[message1_idx]))  #Print all the values
     print ("Second message: "+str(message_list[message2_idx]))
     print ("Duplicate Hash: 0x"+str(seen2[0]))
